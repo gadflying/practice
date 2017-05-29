6fbd341eaed79d5fce6c32f5e576a050956c4f1b
Yarun Luon <yarun.luon@gmail.com>
2017-05-29 13:14:56 -0700

    Add two numbers

---
+++ add-two-numbers.js
@@ -0,0 +1,85 @@
+/*
+2. Add Two Numbers
+https://leetcode.com/problems/add-two-numbers/#/description
+
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = (l1, l2) => {
+  let carry = 0;
+
+  let p1 = l1;
+  let p2 = l2;
+  let pSum;
+  let lSum;
+
+  while (p1 !== null || p2 !== null) {
+    if (!lSum) {
+      pSum = new ListNode();
+      lSum = pSum;
+    } else {
+      pSum.next = new ListNode();
+      pSum = pSum.next;
+    }
+    const value1 = p1 === null ? 0 : p1.val;
+    const value2 = p2 === null ? 0 : p2.val;
+    const sum = value1 + value2 + carry;
+    const digit = sum % 10;
+    carry = Math.floor(sum / 10);
+
+    pSum.val = digit;
+    p1 = p1 && p1.next;
+    p2 = p2 && p2.next;
+  }
+
+  if (carry) {
+    pSum.next = new ListNode(carry);
+  }
+
+  return lSum;
+};
+
+const l1 = (() => {
+  const two = new ListNode(2);
+  const four = new ListNode(4);
+  const three = new ListNode(3);
+  two.next = four;
+  four.next = three;
+  return two;
+})();
+
+const l2 = (() => {
+  const five = new ListNode(5);
+  const six = new ListNode(6);
+  const four = new ListNode(4);
+  five.next = six;
+  six.next = four;
+  return five;
+})();
+
+console.log(addTwoNumbers(l1, l2));
