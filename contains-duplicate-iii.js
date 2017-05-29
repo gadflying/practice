e6a153725645e315a8e2fa1e4c4af1684e942d0c
Yarun Luon <yarun.luon@gmail.com>
2017-05-29 13:55:00 -0700

    Contains duplicate iii

---
+++ contains-duplicate-iii.js
@@ -0,0 +1,23 @@
+/**
+220. Contains Duplicate III
+https://leetcode.com/problems/contains-duplicate-iii/#/description
+
+Given an array of integers, find out whether there are two distinct indices i and j in the array
+such that the absolute difference between nums[i] and nums[j] is at most t and the absolute
+difference between i and j is at most k.
+**/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+const containsNearbyAlmostDuplicate = (nums, k, t) => {
+  const cache = {};
+  const result = nums.some((num, index) => {
+
+  });
+
+  return result;
+};
